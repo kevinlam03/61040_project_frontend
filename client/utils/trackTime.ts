1a451158c22126a6a822f3132de2f21e22158f97
Kevin Lam <lamk5977@gmail.com>
2023-12-02 16:22:43 -0500

    Added showing toast popup if restriction is met during time tracking.

--- client/utils/trackTime.ts
+++ client/utils/trackTime.ts
@@ -1,20 +1,33 @@
+import { useToastStore } from "../stores/toast";
 import { fetchy } from "./fetchy";
 
-// tracking timeUsed for this feature
+// tracking timeUsed and restriction for this feature
 var timeUsed = 0;
+var restriction = 0;
+const toastStore = useToastStore();
 
 export const getTimeUsed = () => {
-    return timeUsed;
+  return timeUsed;
 }
 
 export const getScreenTimeData = async (username: string, feature: string) => {
-    let res;
-    try {
-      res = await fetchy(`api/screenTime/${username}/${feature}`, 'GET');
-    } catch {
-      return;
-    }
-    return res;
+  let res;
+  try {
+    res = await fetchy(`api/screenTime/${username}/${feature}`, 'GET');
+  } catch {
+    return;
+  }
+  return res;
+}
+
+export const getFeatureRestriction = async (feature: string) => {
+  let res;
+  try {
+    res = await fetchy(`api/restriction/time/${feature}`, 'GET');
+  } catch {
+    return;
+  }
+  return res;
 }
 
 /* 
@@ -22,26 +35,42 @@ Start a timer and add to it on delay.
 Returns an object that can be used to stop the execution of the callback.
 */
 export const startTimeTracking = async (username: string, feature: string) => {
-    timeUsed = await getScreenTimeData(username, feature);
+  timeUsed = await getScreenTimeData(username, feature);
+  restriction = await getFeatureRestriction(feature);
 
-    const secondsDelay = 1
-    return setInterval(() => {
-        timeUsed += secondsDelay
-    }, secondsDelay * 1000);
+  if (timeUsed >= restriction) {
+    toastStore.showToast({
+      message: "Time limit exceeded!",
+      style: "error",
+    })
+  }
+
+  const secondsDelay = 1
+
+  return setInterval(() => {
+    timeUsed += secondsDelay
+
+    if (timeUsed === restriction) {
+      toastStore.showToast({
+        message: "Time limit exceeded!",
+        style: "error",
+      })
+    }
+  }, secondsDelay * 1000);
 }
 
 /*
 Uses the argument to stop execution of the callback, update the database screenTime
 */
 export const endTimeTracking = async (interval: any, username: string, feature: string) => {
-    clearInterval(interval);
+  clearInterval(interval);
 
-    try {
-        await fetchy(`api/screenTime/${username}/${feature}`, 'POST');
-    }
-    catch {
-        return;
-    }
+  try {
+    await fetchy(`api/screenTime/${username}/${feature}`, 'POST');
+  }
+  catch {
+    return;
+  }
 }
 
 
